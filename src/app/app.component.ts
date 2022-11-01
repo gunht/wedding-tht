a9109ebd0f4241fdf549471c6453fe79a2aa2861
tunght <tunght@dft.vn>
2022-11-01 07:31:15 +0700

    fix đường dẫn ảnh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,7 @@ export class AppComponent {
 
   playAudio() {
     let audio = new Audio();
-    audio.src = "../assets/mp3/ILoveYou3000-StephaniePoetri.mp3";
+    audio.src = "assets/mp3/ILoveYou3000-StephaniePoetri.mp3";
     audio.load();
     audio.play();
     this.isLoading = false;
