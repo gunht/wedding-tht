90fd451dc6b38e3be460278cf3be3bb00ef8db8c
tunght <tunght@dft.vn>
2022-11-01 07:13:26 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Gallery, GalleryItem, ImageItem, ImageSize, ThumbnailsPosition } from 'ng-gallery';
+import { Lightbox } from 'ng-gallery/lightbox';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wedding-tht';
+  title = 'Wedding';
+  isLoading = false;
+
+  images: GalleryItem[] = [];
+
+  constructor(
+    public gallery: Gallery,
+    private lightbox: Lightbox) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.playAudio();
+
+    this.images = this.items.map(item => new ImageItem({ src: item.src, thumb: item.thumb }));
+
+    this.basicLightbox();
+
+    this.withCustomGalleryConfig();
+  }
+
+  basicLightbox() {
+    this.gallery.ref().load(this.images);
+  }
+
+  withCustomGalleryConfig() {
+    const lightboxGalleryRef = this.gallery.ref('anotherLightbox');
+
+    lightboxGalleryRef.setConfig({
+      imageSize: ImageSize.Contain,
+      thumbPosition: ThumbnailsPosition.Bottom
+    });
+
+    lightboxGalleryRef.load(this.images);
+  }
+
+  playAudio() {
+    let audio = new Audio();
+    audio.src = "../assets/mp3/ILoveYou3000-StephaniePoetri.mp3";
+    audio.load();
+    audio.play();
+    this.isLoading = false;
+  }
+
+  items: any[] = [
+    {
+      src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(1).jpg',
+      thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(1).jpg'
+    },
+    // {
+    //   src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(2).jpg',
+    //   thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(2).jpg'
+    // },
+    // {
+    //   src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(3).jpg',
+    //   thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(3).jpg'
+    // },
+    {
+      src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(11).JPG',
+      thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(11).JPG'
+    },
+    {
+      src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(18).jpg',
+      thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(18).jpg'
+    },
+    // {
+    //   src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(21).JPG',
+    //   thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(21).JPG'
+    // },
+    {
+      src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(22).JPG',
+      thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(22).JPG'
+    },
+    {
+      src: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(24).JPG',
+      thumb: 'https://raw.githubusercontent.com/gunht/img/master/wedding/weeding_(24).JPG'
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryModule } from 'ng-gallery';
+import { LightboxModule  } from  'ng-gallery/lightbox';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    GalleryModule,
+    LightboxModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
